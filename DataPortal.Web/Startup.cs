3269d4aa3d50b8cb04dca3174dc4aafe5a9e37c6
sivaInvincix <siva.seedarla@gmail.com>
2017-06-19 12:35:09 +0530

    Created the Web Project

---
+++ DataPortal.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataPortal.Web.Startup))]
+namespace DataPortal.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
