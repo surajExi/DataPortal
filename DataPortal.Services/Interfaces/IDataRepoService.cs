5085fcd9bf28ce070ec369a6eda3728ab1339f79
sivaInvincix <siva.seedarla@gmail.com>
2017-06-19 13:20:09 +0530

    Added the Project Architecture

---
+++ DataPortal.Services/Interfaces/IDataRepoService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPortal.Services.Interfaces
+{
+    public interface IDataRepoService<T> where T : class
+    {
+        /// <summary>
+        /// Gets all the data based on the Entity type being queried
+        /// </summary>
+        /// <returns></returns>
+        IQueryable<T> GetAll();
+
+        /// <summary>
+        /// Returns the Entity of type T after querying based on the entity ID
+        /// </summary>
+        /// <param name="entityId"></param>
+        /// <returns></returns>
+        T GetByID(long entityId);
+    }
+}
---
+++ DataPortal.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DataPortal.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
